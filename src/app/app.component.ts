f3489c14559f75c45c6252c85bfeac20fc9b1d75
seungyu80 <w0851625@myscc.ca>
2024-09-13 15:45:44 -0400

    Done step 1 to 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from "./models/user";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,4 +14,15 @@ export class AppComponent {
   title = 'Seung Yu';
   course :string = 'Javascript Frameworks';
   section :string = '001'
+
+  titleAssignment2: string = 'User Generation';
+  //user1: User = {userId: 1, firstName: "Seung", lastName: "Yu", phoneNumber: "2261111000", isAdmin: true};
+  userList: User[] = [
+    {userId: 1, firstName: "Seung", lastName: "Yu", phoneNumber: "2261111000", isAdmin: true},
+    {userId: 2, firstName: "Lebron ", lastName: "James", phoneNumber: "2261111001", isAdmin: false},
+    {userId: 3, firstName: "Stephen", lastName: "Curry", phoneNumber: "2261111002", isAdmin: false},
+    {userId: 4, firstName: "Kevin", lastName: "Durant", phoneNumber: "2261111003", isAdmin: false},
+    {userId: 5, firstName: "Shohei", lastName: "Ohtani", phoneNumber: "2261111004", isAdmin: false},
+    {userId: 6, firstName: "Mike", lastName: "Trout", phoneNumber: "2261111005", isAdmin: false}
+  ];
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,5 +4,4 @@ export interface User {
   lastName: string;
   phoneNumber: string;
   isAdmin?: boolean;
-  
 }
