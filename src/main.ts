902c927fc477eecc94993461edaeb69c2f0469e7
SeungYu <w0851625@myscc.ca>
2024-10-07 18:51:34 -0400

    Router set up in main.ts Define a list of routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CustomerListComponent } from './app/customer-list/customer-list.component';
+import { CustomerListItemComponent } from './app/customer-list-item/customer-list-item.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  // Default route
+  {path: '', redirectTo: '/customers', pathMatch: 'full'},
+  {path: 'customers', component: CustomerListComponent},
+  {path: 'customers/:id', component: CustomerListItemComponent}
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
