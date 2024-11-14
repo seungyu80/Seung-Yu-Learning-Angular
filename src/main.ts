5e1d72acbbcf49e22aa0554e58be2c516bb8dc70
Seung Yu <w0851625@myscc.ca>
2024-11-14 14:23:48 -0500

    Lazy loading updated

--- src/main.ts
+++ src/main.ts
@@ -13,18 +13,22 @@ import { importProvidersFrom } from '@angular/core';
 
 const routes: Routes = [
   // Default route
-  {path: '', redirectTo: '/customers', pathMatch: 'full'},
-  {path: 'customers', component: CustomerListComponent},
-  {path: 'customers/:customerID', component: CustomerListItemComponent},
-  {path: 'modify-customer', component: ModifyListItemComponent},
-  {path: '**', component: PageNotFoundComponent} // Wildcard route for a 404 page
+  {path: '', redirectTo: '/customers', pathMatch: 'full'}, // Default route
+  {path: 'customers', component: CustomerListComponent},  //eagrly loaded
+  // Lazy loaded
+  {path: 'customers/:customerID', loadComponent: () => 
+    import('./app/customer-list-item/customer-list-item.component').then(m => m.CustomerListItemComponent)},
+  {path: 'modify-customer', loadComponent: () => 
+    import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)},
+  {path: '**', loadComponent: () => 
+    import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)}
 ];
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 500}))
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 100}))
   ],
 }).catch((err) => console.error(err));
 // bootstrapApplication(AppComponent, appConfig)
